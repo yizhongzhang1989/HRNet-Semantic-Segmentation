1f01b0283393e6037c2d25ab34371b0f7a48fac7
wangfys <fyswang@gmail.com>
2020-09-24 14:48:52 +0800

    Update analysisResult.py

--- tools/analysisResult.py
+++ tools/analysisResult.py
@@ -1,13 +1,12 @@
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import shutil
 
-analysis_simplified = False
-analysis_compressed = True
-data_dir = "D:/panorama/merge"
-result_dir = "D:/testNet/result"
+data_dir = "D:/panorama/merge720x540"
+result_dir = "D:/testNet/test_val_results"
 output_dir = "D:/testNet"
+topK = 20
 
 compress_map = {
     0: 0,
@@ -35,107 +34,37 @@ with open("colorMap.txt", "r") as f:
         tmp = line.split(" ")
         label = int(tmp[1])
         r, g, b = int(tmp[2]), int(tmp[3]), int(tmp[4])
-        if analysis_compressed:
-            colormap[compress_map[label]] = np.array([r,g,b], dtype=np.int32)
-        else:
-            colormap[label] = np.array([r,g,b], dtype=np.int32)
+        colormap[label] = np.array([r,g,b], dtype=np.int32)
 
-def plot_confusion_matrix(cm):
-    import matplotlib.pyplot as plt
-    label_map = {
-            0: "ground",
-            1: "ceiling",
-            2: "wall",
-            3: "pillar",
-            4: "door",
-            5: "window",
-            6: "stairs",
-            7: "escalator",
-            8: "elevator",
-            10: "layered_shelf",
-            11: "table_shelf",
-            13: "tall_freezer",
-            14: "short_freezer",
-            15: "cashier",
-            20: "hanging_object",
-            30: "other_object",
-            40: "removed_object"
-        }
-    if cm.shape[0] == 41:
-        class_names = []
-        slices = []
-        for i in range(41):
-            if i in label_map:
-                slices.append(True)
-                class_names.append(label_map[i])
-            else:
-                slices.append(False)
-        cm = cm[slices]
-        cm = cm[:,slices]
-    elif cm.shape[0] == 17:
-        class_names = []
-        for key in label_map:
-            class_names.append(label_map[key])
-    
-    figure = plt.figure(figsize=(12, 12))
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    plt.title("Confusion matrix")
-    plt.colorbar()
-    tick_marks = np.arange(len(class_names))
-    plt.xticks(tick_marks, class_names, rotation=45)
-    plt.yticks(tick_marks, class_names)
-
-    threshold = cm.max() / 2.
-    normalized = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            color = "white" if cm[i, j] > threshold else "black"
-            plt.text(j, i, normalized[i, j], horizontalalignment="center", color=color)
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    return figure
-
-if analysis_simplified:
-    matrix = np.zeros([4, 4], dtype=np.int64)
-elif analysis_compressed:
-    matrix = np.zeros([17, 17], dtype=np.int64)
-else:
-    matrix = np.zeros([41, 41], dtype=np.int64)
+if os.path.exists("%s/easy" % output_dir):
+    shutil.rmtree("%s/easy" % output_dir)   
+if os.path.exists("%s/hard" % output_dir):
+    shutil.rmtree("%s/hard" % output_dir)
+os.mkdir("%s/easy" % output_dir)
+os.mkdir("%s/hard" % output_dir)
 
 all_results = os.listdir(result_dir)
 errors = []
 for filename in all_results:
-    #print(filename)
+    print(filename)
     predict = cv2.imread("%s/%s" % (result_dir, filename), cv2.IMREAD_GRAYSCALE)
-    if analysis_simplified:
-        truth = cv2.imread("%s/simplify/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
-    elif analysis_compressed:
-        truth = cv2.imread("%s/compress/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
-    else:
-        truth = cv2.imread("%s/label/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
+    truth = cv2.imread("%s/label/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
     h, w = predict.shape
     error = 0
+    for key in compress_map:
+        if key != compress_map[key]:
+            truth[truth == key] = compress_map[key]
     error = (predict != truth).sum()
     errors.append(error)
 
-#figure = plot_confusion_matrix(matrix)
-#plt.savefig("%s/analysis.png" % output_dir)
-
 errors = np.array(errors)
 
-for i in np.argsort(-errors)[:20]:
+for i in np.argsort(-errors)[:topK]:
     filename = all_results[i]
     name = filename.split(".")[0]
     image = cv2.imread("%s/image/%s.jpg" % (data_dir, name), cv2.IMREAD_COLOR)
     predict = cv2.imread("%s/%s" % (result_dir, filename), cv2.IMREAD_GRAYSCALE)
-    if analysis_simplified:
-        truth = cv2.imread("%s/simplify/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
-    elif analysis_compressed:
-        truth = cv2.imread("%s/compress/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
-    else:
-        truth = cv2.imread("%s/label/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
+    truth = cv2.imread("%s/label/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
     h, w = truth.shape
     RGB_label = np.zeros([h, w, 3], dtype=np.uint8)
     for key in colormap:
@@ -148,17 +77,12 @@ for i in np.argsort(-errors)[:20]:
     RGB_label = RGB_label[:,:,::-1]
     cv2.imwrite("%s/hard/%s_predict.png" % (output_dir, name), RGB_label)
 
-for i in np.argsort(errors)[:20]:
+for i in np.argsort(errors)[:topK]:
     filename = all_results[i]
     name = filename.split(".")[0]
     image = cv2.imread("%s/image/%s.jpg" % (data_dir, name), cv2.IMREAD_COLOR)
     predict = cv2.imread("%s/%s" % (result_dir, filename), cv2.IMREAD_GRAYSCALE)
-    if analysis_simplified:
-        truth = cv2.imread("%s/simplify/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
-    elif analysis_compressed:
-        truth = cv2.imread("%s/compress/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
-    else:
-        truth = cv2.imread("%s/label/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
+    truth = cv2.imread("%s/label/%s" % (data_dir, filename), cv2.IMREAD_GRAYSCALE)
     h, w = truth.shape
     RGB_label = np.zeros([h, w, 3], dtype=np.uint8)
     for key in colormap:
